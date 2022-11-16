765d30438fd8aae4c1454a56fab230cf0f450dbc
Jihad7898 <107761291+Barryeasy90@users.noreply.github.com>
2022-11-16 22:50:30 +0000

    data file added

---
+++ data.js
@@ -0,0 +1,25 @@
+function organizationsData() {
+  return [
+    {
+      nameOfOrg: "NHS",
+      imgeofOrg:
+        "https://s3-eu-west-1.amazonaws.com/files.mylmc.co.uk/websiteimages/43/6162/NHS.jpeg",
+      aboutOrg:
+        "<p>The NHS stands for the National Health Service</p> <p> It refers to the Government-funded medical and health care services that everyone living in the UK can use without being asked to pay the full cost of the service.</p>",
+    },
+    {
+      nameOfOrg: "CYF",
+      imgeofOrg:
+        "https://www.fatbeehive.com/wp-content/uploads/2021/07/CYF-illustration.png",
+      aboutOrg:
+        "<p>CodeYourFuture (CYF)</p><p> is a UK based non-profit organisation that trains some of the most deprived members of society to become web developers and helps them to find work in the tech industry CYF students are trained in full-stack web development by volunteers from the tech industry, putting a strong emphasis on collaboration and product development through tech projects</p>",
+    },
+    {
+      nameOfOrg: "RedCross",
+      imgeofOrg:
+        "https://d1d8vslyhr7rdg.cloudfront.net/Pictures/1024x536/P/web/q/v/r/britishredcross_900x600logo_216716.png",
+      aboutOrg:
+        "<p>Support people in crisis this winter</p> <p>Please help us reach those most in need in the UK and overseas. Together, we are the world's emergency responders.</p>",
+    },
+  ];
+}
