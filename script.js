26d1932e4f2bc302e5998140b20991fca7629ddd
Ahmed Mohamed <105979851+AhmedMohamed809@users.noreply.github.com>
2022-11-17 01:02:00 +0000

    add all staf

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 arrayData = organizationsData();
-const container = document.getElementById("container");
-const headerOforg = document.querySelectorAll("#header-org");
-const imageOfOrg = document.getElementById("img-org");
-const paragraph = document.getElementById("par-org");
+// const container = document.getElementById("container");
+const mainContainer = document.getElementById("mainContainer");
 arrayData.forEach((e) => {
+  const headerOforg = document.createElement("h1");
+  const imageOfOrg = document.createElement("img");
+  const container = document.createElement("div");
+  container.id = "container";
+  imageOfOrg.id = "imges";
+  const paragraph = document.createElement("p");
+  const buttonOfOrg = document.createElement("button");
+  buttonOfOrg.id = "donateBt";
   headerOforg.innerText = e.nameOfOrg;
-});
-for (let i = 0; i < arrayData.length; i++) {
+  imageOfOrg.setAttribute("src", e.imgeofOrg);
+  paragraph.innerHTML = e.aboutOrg;
+  buttonOfOrg.innerText = "Donate";
+  console.log(e);
+  mainContainer.appendChild(container);
   container.appendChild(headerOforg);
   container.appendChild(imageOfOrg);
   container.appendChild(paragraph);
-}
-console.log("hi");
+  container.appendChild(buttonOfOrg);
+});
+
+window.onload = arrayData;
