53a40e31f2863b05ca39e443f7c0b6816e87ad5b
fatih-celebi <celebifatih.c1@gmail.com>
2022-11-17 23:55:25 +0000

    Add Payment Page

--- script.js
+++ script.js
@@ -5,21 +5,23 @@ arrayData.forEach((e) => {
   const headerOforg = document.createElement("h1");
   const imageOfOrg = document.createElement("img");
   const container = document.createElement("div");
+  const aLink = document.createElement("a")
   container.id = "container";
   imageOfOrg.id = "imges";
   const paragraph = document.createElement("p");
   const buttonOfOrg = document.createElement("button");
   buttonOfOrg.id = "donateBt";
+  aLink.setAttribute("href", "paymentPage.html");
   headerOforg.innerText = e.nameOfOrg;
   imageOfOrg.setAttribute("src", e.imgeofOrg);
   paragraph.innerHTML = e.aboutOrg;
   buttonOfOrg.innerText = "Donate";
-  console.log(e);
   mainContainer.appendChild(container);
   container.appendChild(headerOforg);
   container.appendChild(imageOfOrg);
   container.appendChild(paragraph);
-  container.appendChild(buttonOfOrg);
+  container.appendChild(aLink);
+  aLink.appendChild(buttonOfOrg)
 });
 
 window.onload = arrayData;
