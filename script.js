be6499c7eb24be8d98321e7517bd68505ccc6f0e
Ahmed Mohamed <105979851+AhmedMohamed809@users.noreply.github.com>
2022-11-17 00:10:41 +0000

    hi

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-
+arrayData = organizationsData();
+const container = document.getElementById("container");
+const headerOforg = document.querySelectorAll("#header-org");
+const imageOfOrg = document.getElementById("img-org");
+const paragraph = document.getElementById("par-org");
+arrayData.forEach((e) => {
+  headerOforg.innerText = e.nameOfOrg;
+});
+for (let i = 0; i < arrayData.length; i++) {
+  container.appendChild(headerOforg);
+  container.appendChild(imageOfOrg);
+  container.appendChild(paragraph);
+}
+console.log("hi");
